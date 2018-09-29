4c13165c4efbcaa85bec354d6dbe4f2b0411a487
FarmerRobbie <farmerrobbie@freshsalad.today>
2018-09-29 14:21:34 -0700

    Finished opening and closing actuator.

--- flask_server/bark_door_app.py
+++ flask_server/bark_door_app.py
@@ -1,57 +1,49 @@
-from flask import Flask, render_template, request, jsonify
-
-import os
-import sys
+import RPi.GPIO as GPIO
 import time
 
-LIB_PATH = os.environ['LADYBUG_LIB_PATH']
-sys.path.append(LIB_PATH)
-from RFM69_messages_lib import RFM69Messages
-
-rf69 = RFM69Messages()
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+
+open_pin = 7
+close_pin = 12
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(open_pin, GPIO.OUT)
+GPIO.setup(close_pin, GPIO.OUT)
+
+
+def turn_on_off_actuator(open):
+    """1 = open, 0 = close"""
+    if open == 1 or open == 0:
+        if open == 1:
+            pin = open_pin
+        else:
+            pin = close_pin
+        GPIO.output(pin, True)
+        time.sleep(10)
+        GPIO.output(pin, False)
+        return True
+    return False
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
-    # return('hello')
 
 
 @app.route('/get_open_close', methods=['POST'])
 def get_open_close():
-    open_or_close = request.get_json()
-    # open_or_close will be {open:1} for open or {open:0} for close
-    send_open_or_close(open_or_close['open'])
-    resp = jsonify(success=True)
+    which_button = request.get_json()
+    if turn_on_off_actuator(which_button['open']):
+        resp = jsonify(success=True)
+    else:
+        resp = jsonify(success=False)
     return resp
-
-
-def send_open_or_close(open):
-    packet_to_send = [rf69._ACTUATOR,open]
-    received_packet = False
-    while True:
-        if received_packet:
-            break
-        rf69.radio.send(bytearray(packet_to_send))
-        #rf69.radio.send('Hello world!\r\n')
-        print('Sent hello world message!')
-        time.sleep(1)
-    # rf69.radio.send(bytearray(packet_to_send))
-    rf69.radio.receive_begin(keep_listening=True, callback=receive_done)
-
-def receive_done(packet):
-    received_packet = True
-    print("received packet {}".format(packet))
-
 
 
 if __name__ == "__main__":
     # When debug=True, the debug service restarts after changes are made.
     # This is very handy!
-    # Using Remot3.it.  Following lead from this question/answer:
-    # http://forum.weaved.com/t/streaming-video-on-web-page/292/2
     app.run(host='raspberrypi.home', port=8519, debug=True, threaded=True)
-# host = localhost when running on mac
-# app.run(debug=True, host='localhost', port=9999)
+    # host = localhost when running on mac
+    # app.run(debug=True, host='localhost', port=9999)
