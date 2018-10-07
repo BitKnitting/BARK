32353430e5b72f8dfc9b2c41a91f9d0ce7c1d3f8
FarmerRobbie <farmerrobbie@freshsalad.today>
2018-10-07 11:02:42 -0700

    Evolved as started using as prototype.

--- flask_server/bark_door_app.py
+++ flask_server/bark_door_app.py
@@ -5,23 +5,28 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-open_pin = 7
-close_pin = 12
+open_pin = 12
+close_pin = 7
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(open_pin, GPIO.OUT)
 GPIO.setup(close_pin, GPIO.OUT)
 
 
-def turn_on_off_actuator(open):
-    """1 = open, 0 = close"""
-    if open == 1 or open == 0:
-        if open == 1:
+def action_on_actuator(action_to_do):
+    """0 = close, 1 = open, 2 = stop"""
+    if action_to_do == 1 or action_to_do == 0:
+        if action_to_do == 1:
             pin = open_pin
         else:
             pin = close_pin
         GPIO.output(pin, True)
-        time.sleep(10)
+        time.sleep(28)
         GPIO.output(pin, False)
+        return True
+    #  Doesn't hurt to turn off both pins..
+    if action_to_do == 2:
+        GPIO.output(close_pin, False)
+        GPIO.output(open_pin, False)
         return True
     return False
 
@@ -34,7 +39,7 @@ def home():
 @app.route('/get_open_close', methods=['POST'])
 def get_open_close():
     which_button = request.get_json()
-    if turn_on_off_actuator(which_button['open']):
+    if action_on_actuator(which_button['action']):
         resp = jsonify(success=True)
     else:
         resp = jsonify(success=False)
